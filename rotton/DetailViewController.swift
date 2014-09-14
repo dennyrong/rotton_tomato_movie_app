6c693621075278e0d865e5f24c829af644a99ebc
dennyrong <denny.rongkun@gmail.com>
2014-09-14 15:14:42 -0700

    rotton photato update

--- rotton/DetailViewController.swift
+++ rotton/DetailViewController.swift
@@ -19,6 +19,15 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var MovieTitle: UILabel!
     @IBOutlet weak var MovieDetail: UILabel!
     
+    @IBOutlet weak var Year: UILabel!
+    
+    
+    @IBOutlet weak var MPAA: UILabel!
+    
+    @IBOutlet weak var C_rating: UILabel!
+    
+    @IBOutlet weak var A_rating: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,13 +37,29 @@ class DetailViewController: UIViewController {
         
         var posters = movie["posters"] as NSDictionary
         var posterURL = posters["thumbnail"] as String
+        var year = movie["year"] as Int
+        var mpaa = movie["mpaa_rating"] as? String
+        // get movie rating
+        var ratings = movie["ratings"] as NSDictionary
+        
+        
+        Year.text = "\(year)"
+        MPAA.text = mpaa
+        
+        println("year:\(year), mpaa:\(mpaa)")
+        
+        var c_rating = ratings["critics_score"] as Float
+        var a_rating = ratings["audience_score"] as Float
+        
+        C_rating.text = "\(c_rating)%"
+        A_rating.text = "\(a_rating)%"
+        
+        println("c rating:\(c_rating), a_raitng:\(a_rating)")
+        
         moviePoster.setImageWithURL(NSURL(string: posterURL))
-        
         nvbar.title = movie["title"] as? String
-
         MovieTitle.text = movie["title"] as? String
-        
-        MovieDetail.text = movie["synopsis"] as? String
+        MovieDetail.text =  movie["synopsis"] as? String
         
         // Do any additional setup after loading the view.
     }
